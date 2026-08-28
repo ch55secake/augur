8373bebb948c88251ecfd85912f6e086e4313e93
ch55secake <oscardjbackup@gmail.com>
2026-08-28 16:31:31 +0100

    feat: add nix development flake

---
+++ flake.nix
@@ -0,0 +1,54 @@
+{
+  description = "Watch over active SSH connections and terminate unrecognized ones";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      systems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-linux"
+      ];
+
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      packages = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          default = pkgs.buildGoModule {
+            pname = "augur";
+            version = "0.1.0";
+            src = ./.;
+            go = pkgs.go_1_26;
+            vendorHash = null;
+          };
+        }
+      );
+
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          default = pkgs.mkShell {
+            packages = [ pkgs.go_1_26 ];
+          };
+        }
+      );
+
+      formatter = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        pkgs.nixfmt
+      );
+    };
+}
